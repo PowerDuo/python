a2e30c87d37cfe76cb1145e025ee7d48351aae33
avk29 <66971874+avk29@users.noreply.github.com>
2020-10-05 11:40:16 +0530

    Delete factorial.py

--- factorial.py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-    if n == 1:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-m = int(input('Enter number whose factorial you wish to calculate: '))
-print(factorial(m))
